d24416452a265eebe037640c9ee3e7b11f09d7d1
somerongit <someron.bakuli05@gmail.com>
2021-05-18 20:26:54 +0530

    CRUD is done

--- CRUD/views.py
+++ CRUD/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .serializers import TaskSerializer
+from .models import Task
 
 @api_view(['GET'])
 def apiOverview(request):
@@ -15,3 +16,41 @@ def apiOverview(request):
         'Delete':'/task-delete/<str:pk>'
     }
     return Response(api_urls)
+
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializers = TaskSerializer(tasks, many=True)
+    return Response(serializers.data)
+
+@api_view(['GET'])
+def taskDetail(request,pk):
+    tasks = Task.objects.get(id=pk)
+    serializers = TaskSerializer(tasks, many=False)
+    return Response(serializers.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+    
+    serializers = TaskSerializer(data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+
+    return Response(serializers.data)
+
+@api_view(['POST'])
+def taskUpdate(request,pk):
+    tasks = Task.objects.get(id=pk)
+    serializers = TaskSerializer(instance=tasks, data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+        
+    return Response(serializers.data)
+
+@api_view(['DELETE'])
+def taskDelete(request,pk):
+    tasks = Task.objects.get(id=pk)
+    tasks.delete()
+        
+    return Response(tasks)
